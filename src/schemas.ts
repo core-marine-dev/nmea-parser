87662940e4ee197dd2ee4aa81f485b937e8bacff
Cristóbal Contreras Rubio <crisconru@gmail.com>
2023-10-14 17:38:04 +0200

    defined yaml file

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,34 +1,45 @@
 import { z } from 'zod'
-import { CHECKSUM_LENGTH, DELIMITER, END_FLAG, MINIMAL_LENGTH, START_FLAG } from './constants'
+import { CHECKSUM_LENGTH, DELIMITER, END_FLAG, MINIMAL_LENGTH, SEPARATOR, START_FLAG } from './constants'
 import { stringChecksumToNumber, getChecksum } from './checksum'
 
 export const StringSchema = z.string()
+export const StringArraySchema = z.array(StringSchema)
 export const BooleanSchema = z.boolean()
 export const NumberSchema = z.number()
 export const NaturalSchema = NumberSchema.int().positive()
 
 
 // NMEA
+export const NMEASentenceSchema = z.object({
+  frame: StringSchema,
+  emitter: StringSchema,
+  fields: z.array(StringSchema),
+  checksum: NumberSchema,
+})
+
 export const NMEALineSchema = StringSchema.superRefine((line, ctx) => {
   if (line.length < MINIMAL_LENGTH) {
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
       message: 'Invalid NMEA line -> it doesn\'t contain any data'
     })
+    return
   }
   // Check Start FLAG
-  if (StringSchema.startsWith(START_FLAG).safeParse(line).success) {
+  if (!StringSchema.startsWith(START_FLAG).safeParse(line).success) {
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
       message: 'Invalid NMEA line -> it doesn\'t start with $'
     })
+    return
   }
   // Check End Flag
-  if (StringSchema.endsWith(END_FLAG).safeParse(line).success) {
+  if (!StringSchema.endsWith(END_FLAG).safeParse(line).success) {
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
       message: 'Invalid NMEA line -> it doesn\'t end with CRLF'
     })
+    return
   }
   const frame = line.slice(1, - END_FLAG.length).split(DELIMITER)
   // Check Delimiter
@@ -37,6 +48,7 @@ export const NMEALineSchema = StringSchema.superRefine((line, ctx) => {
       code: z.ZodIssueCode.custom,
       message: `Invalid NMEA line -> it doesn't contain just one DELIMITER ${DELIMITER}`
     })
+    return
   }
   // Check Checksum length
   const [data, checksum] = frame
@@ -45,6 +57,7 @@ export const NMEALineSchema = StringSchema.superRefine((line, ctx) => {
       code: z.ZodIssueCode.custom,
       message: `Invalid NMEA line -> checksum has not just two characters => CHECKSUM = ${checksum}`
     })
+    return
   }
   const dataChecksumNumber = getChecksum(data)
   const checksumNumber = stringChecksumToNumber(checksum)
@@ -53,12 +66,88 @@ export const NMEALineSchema = StringSchema.superRefine((line, ctx) => {
       code: z.ZodIssueCode.custom,
       message: `Invalid NMEA line -> calculated checksum ${dataChecksumNumber} != frame checksum ${checksumNumber}`
     })
+    return
   }
+  if (data.indexOf(SEPARATOR) === -1) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Invalid NMEA line -> does not include any field (no ${SEPARATOR} in the frame)`
+    })
+  }
+}).transform(line => {
+  const frame = line.slice(START_FLAG.length, - END_FLAG.length)
+  const [data, cs ] = frame.split(DELIMITER)
+  const checksum = stringChecksumToNumber(cs)
+  const [emitter, ...fields] = data.split(SEPARATOR)
+  return NMEASentenceSchema.parse({ frame, emitter, fields, checksum })
 })
 
-export const NMEASentenceSchema = z.object({
-  emitter: StringSchema,
-  fields: z.array(StringSchema),
-  checksum: NumberSchema,
-  sentence: StringSchema
+// PROTOCOLS
+export const FieldTypeSchema = z.union([
+  // Numbers
+  z.literal('char'), z.literal('uint8'),
+  z.literal('signed char'), z.literal('int8'),
+
+  z.literal('unsigned short'), z.literal('uint16'),
+  z.literal('short'), z.literal('int16'),
+  
+  z.literal('unsigned int'), z.literal('uint32'),
+  z.literal('int'), z.literal('int32'),
+  
+  z.literal('unsigned long'), z.literal('uint64'),
+  z.literal('long'), z.literal('int64'),
+  
+  z.literal('float'), z.literal('float32'),
+  z.literal('double'), z.literal('float64'),
+
+  z.literal('number'),
+  // Strings
+  z.literal('string'),
+  // Boolean
+  z.literal('bool'),
+  z.literal('boolean'),
+])
+
+export const FieldSchema = z.object({
+  name: StringSchema,
+  type: FieldTypeSchema,
+  units: StringSchema.optional(),
+  note: StringSchema.optional()
 })
+
+export const SentenceSchema = z.object({
+  sentence: StringSchema,
+  description: StringSchema.optional(),
+  fields: z.array(FieldSchema)
+})
+
+export const VersionSchema = z.custom<`${number}.${number}.${number}`>(val => {
+  const parsed = StringSchema.safeParse(val)
+  if (!parsed.success) return false
+  const fields = parsed.data.split('.')
+  if (fields.length !== 3) return false
+  return fields.every(field => !isNaN(field))
+}).or(
+  z.custom<`${number}.${number}`>(val => {
+    const parsed = StringSchema.safeParse(val)
+    if (!parsed.success) return false
+    const fields = parsed.data.split('.')
+    if (fields.length !== 2) return false
+    return fields.every(field => !isNaN(field))
+  })
+).or(
+  z.custom<`${number}`>(val => {
+    const parsed = StringSchema.safeParse(val)
+    if (!parsed.success) return false
+    return !isNaN(parsed.data)
+  })
+)
+
+export const ProtocolSchema = z.object({
+  name: StringSchema,
+  version: VersionSchema.optional(),
+  standard: BooleanSchema.default(false),
+  sentences: z.array(SentenceSchema)
+})
+
+export const ProtocolsFileSchema = z.object({ protocols: z.array(ProtocolSchema) })
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,23 @@
 import { z } from 'zod'
-import { NMEALineSchema, NMEASentenceSchema } from './schemas'
+import { NMEALineSchema, NMEASentenceSchema, ProtocolSchema, ProtocolsFileSchema, SentenceSchema } from './schemas'
 
 // NMEA
-export type NMEALine = z.infer<typeof NMEALineSchema>
 export type NMEASentence = z.infer<typeof NMEASentenceSchema>
+export type NMEALine = z.infer<typeof NMEALineSchema>
+
+// Parser
+export type ValidFrameResponse = {
+  valid: boolean,
+  data: string
+}
+
+export interface NMEAParser {
+  getProtocols(): string[],
+  addProtocol(protocol: any): void,
+  parseData(data: string): any[],
+}
+
+// PROTOCOLS
+export type Sentence = z.infer<typeof SentenceSchema>
+export type Protocol = z.infer<typeof ProtocolSchema>
+export type ProtocolsFile = z.infer<typeof ProtocolsFileSchema>
