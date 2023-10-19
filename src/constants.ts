59000f108850e09169552d72f7bb563a52408196
Cristóbal Contreras Rubio <crisconru@gmail.com>
2023-10-19 09:30:15 +0200

    Added max memory length

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,7 @@ export const DELIMITER = '*'
 export const CHECKSUM_LENGTH = 2
 export const END_FLAG = '\r\n'
 export const MINIMAL_LENGTH = START_FLAG.length + DELIMITER.length + CHECKSUM_LENGTH + END_FLAG.length
+
+// PARSER
+export const MAX_CHARACTERS = 1024
+export const MAX_NMEA_CHARACTERS = 82
