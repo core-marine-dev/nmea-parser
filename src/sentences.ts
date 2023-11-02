6d706337c5e142acdf112b5819a0b29465214d3b
Cristóbal Contreras Rubio <crisconru@gmail.com>
2023-11-02 14:14:06 +0100

    tested get number values integers signed or unsigned until 64bits

---
+++ src/sentences.ts
@@ -0,0 +1,72 @@
+import { getChecksum, numberChecksumToString } from "./checksum";
+import { Data, FieldType, NMEALike, StoredSentence } from "./types";
+
+
+export const getNumberValue = (type: FieldType): Data => {
+  const seed = Math.random() * Math.pow(10, 10)
+  switch (type) {
+    case 'uint8':
+    case 'char':
+      return (new Uint8Array([seed]))[0]
+
+    case 'int8':
+    case 'signed char':
+      return (new Int8Array([seed]))[0]
+
+    case 'uint16':
+    case 'unsigned short':
+      return (new Uint16Array([seed]))[0]
+
+    case 'int16':
+    case 'short':
+      return (new Int16Array([seed]))[0]
+
+    case 'uint32':
+    case 'unsigned int':
+      return (new Uint32Array([seed]))[0]
+
+    case 'int32':
+    case 'int':
+      return (new Int32Array([seed]))[0]
+
+    case 'uint64':
+    case 'unsigned long':
+      return Number((new BigUint64Array([BigInt(Math.floor(seed))]))[0])
+        
+    case 'int64':
+    case 'long':
+      return Number((new BigInt64Array([BigInt(Math.floor(seed))]))[0])
+
+    case 'float32':
+    case 'float':
+      return (new Float32Array([seed]))[0]
+
+    case 'float64':
+    case 'double':
+      return (new Float64Array([seed]))[0]
+  }
+  throw Error('invalid type')
+}
+
+export const getValue = (type: FieldType): Data => {
+  switch (type) {
+    case 'bool':
+    case 'boolean':
+      return Math.random() > 0.5
+    case 'string':
+      return Math.random().toString(36).substring(2)
+  }
+  return getNumberValue(type)
+}
+
+export const generateSentence = (model: StoredSentence): NMEALike => {
+  let sentence = `$${model.sentence}`
+  model.fields.forEach(field => {
+    const value = getValue(field.type)
+    sentence += `,${value}`
+  })
+  const cs = getChecksum(sentence.slice(1))
+  const checksum = numberChecksumToString(cs)
+  sentence += `*${checksum}\r\n`
+  return sentence
+}
---
+++ tests/sentences.test.ts
@@ -0,0 +1,109 @@
+import { describe, expect, test } from 'vitest'
+import { getNumberValue } from '../src/sentences'
+import { FieldType } from '../src/types'
+import { IntegerSchema, NaturalSchema, NumberSchema } from '../src/schemas'
+
+describe('getNumberValue', () => {
+  const MIN_UINT8 = 0b0000_0000
+  const MAX_UINT8 = 0b1111_1111
+  const UINT8_LIMITS = Uint8Array.from([MIN_UINT8, MAX_UINT8])
+
+  const MIN_INT8 = 0b1000_0000
+  const MAX_INT8 = 0b0111_1111
+  const INT8_LIMITS = Int8Array.from([MIN_INT8, MAX_INT8])
+
+  const MIN_UINT16 = 0b0000_0000_0000_0000
+  const MAX_UINT16 = 0b1111_1111_1111_1111
+  const UINT16_LIMITS = Uint16Array.from([MIN_UINT16, MAX_UINT16])
+  
+  const MIN_INT16 = 0b1000_0000_0000_0000
+  const MAX_INT16 = 0b0111_1111_11111111
+  const INT16_LIMITS = Int16Array.from([MIN_INT16, MAX_INT16])
+  
+  const MIN_UINT32 = 0b0000_0000_0000_0000_0000_0000_0000_0000
+  const MAX_UINT32 = 0b1111_1111_1111_1111_1111_1111_1111_1111
+  const UINT32_LIMITS = Uint32Array.from([MIN_UINT32, MAX_UINT32])
+  
+  const MIN_INT32 = 0b1000_0000_0000_0000_0000_0000_0000_0000
+  const MAX_INT32 = 0b0111_1111_1111_1111_1111_1111_1111_1111
+  const INT32_LIMITS = Int32Array.from([MIN_INT32, MAX_INT32])
+
+  const MIN_UINT64 = BigInt(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+  const MAX_UINT64 = BigInt(0b11111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111)
+  const UINT64_LIMITS = BigUint64Array.from([MIN_UINT64, MAX_UINT64])
+
+  const MIN_INT64 = BigInt(0b1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000)
+  const MAX_INT64 = BigInt(0b01111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111)
+  const INT64_LIMITS =  BigInt64Array.from([MIN_INT64, MAX_INT64])
+
+  test('invalid number type', () => {
+    expect(() => getNumberValue('a' as FieldType)).toThrowError()
+  })
+
+  test('uint8', () => {
+    ['uint8', 'char'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = NaturalSchema.min(UINT8_LIMITS[0]).max(UINT8_LIMITS[1]).parse(value)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test('uint16', () => {
+    ['uint16', 'unsigned short'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = NaturalSchema.min(UINT16_LIMITS[0]).max(UINT16_LIMITS[1]).parse(value)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test('uint32', () => {
+    ['uint32', 'unsigned int'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = NaturalSchema.min(UINT32_LIMITS[0]).max(UINT32_LIMITS[1]).parse(value)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test.todo('uint64', () => {
+    ['uint64', 'unsigned long'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const bigValue = BigInt(value as number)
+      const bigExpected = NaturalSchema.min(Number(UINT64_LIMITS[0])).max(Number(UINT64_LIMITS[1])).parse(bigValue)
+      const expected = Number(bigExpected)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test('int8', () => {
+    ['int8', 'signed char'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = IntegerSchema.min(INT8_LIMITS[0]).max(INT8_LIMITS[1]).parse(value)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test('int16', () => {
+    ['int16', 'short'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = IntegerSchema.min(INT16_LIMITS[0]).max(INT16_LIMITS[1]).parse(value)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test('int32', () => {
+    ['int32', 'int'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = IntegerSchema.min(INT32_LIMITS[0]).max(INT32_LIMITS[1]).parse(value)
+      expect(value).toBe(expected)
+    })
+  })
+
+  test.todo('int64', () => {
+    ['int64', 'long'].forEach(type => {
+      const value = getNumberValue(type as FieldType)
+      const expected = NumberSchema.min(Number(INT64_LIMITS[0])).max(Number(INT64_LIMITS[1])).parse(value)
+      expect(value).toBe(expected)
+    })
+
+  })
+})
