5c9dccbc3bf6107395c56cdb305c4f2a9fa95082
Cristóbal Contreras Rubio <crisconru@gmail.com>
2023-11-02 10:42:41 +0100

    generate checksum string for sentences

--- src/checksum.ts
+++ src/checksum.ts
@@ -2,3 +2,11 @@
 export const getChecksum = (data: string): number => Array.from(data).reduce((acc, cur) => acc ^ cur.charCodeAt(0), 0)
 
 export const stringChecksumToNumber = (checksum: string) => parseInt(checksum, 16)
+
+export const numberChecksumToString = (checksum: number) => {
+  const num0 = checksum & 0b0000_1111
+  const num1 = (checksum & 0b1111_0000) >>> 4
+  const char0 = num0.toString(16).toUpperCase()
+  const char1 = num1.toString(16).toUpperCase()
+  return `${char1}${char0}`
+}
---
+++ tests/checksum.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from "vitest"
+import { CHECKSUM_LENGTH, DELIMITER, END_FLAG} from '../src/constants'
+import { getChecksum, numberChecksumToString, stringChecksumToNumber } from '../src/checksum'
+
+
+const TEST_SENTENCES = [
+  '$GPGGA,074844.199,3652.514,N,00225.679,W,1,12,1.0,0.0,M,0.0,M,,*79\r\n',
+  '$GPGSA,A,3,01,02,03,04,05,06,07,08,09,10,11,12,1.0,1.0,1.0*30\r\n',
+  '$GPRMC,074844.199,A,3652.514,N,00225.679,W,30386.3,045.0,021123,000.0,W*62\r\n',
+  '$GPGGA,074845.199,3659.100,N,00219.087,W,1,12,1.0,0.0,M,0.0,M,,*7A\r\n',
+  '$GPGSA,A,3,01,02,03,04,05,06,07,08,09,10,11,12,1.0,1.0,1.0*30\r\n',
+  '$GPRMC,074845.199,A,3659.100,N,00219.087,W,30386.3,045.0,021123,000.0,W*61\r\n',
+]
+
+const EXPECTED_CHECKSUMS = [ 121, 48, 98, 122, 48, 97 ]
+
+test('getChecksum', () => {
+  TEST_SENTENCES.forEach((sentence, index) => {
+    const data = sentence.slice(1, - (DELIMITER.length + CHECKSUM_LENGTH + END_FLAG.length))
+    const checksum = getChecksum(data)
+    expect(checksum).toBe(EXPECTED_CHECKSUMS[index])
+  })
+})
+
+test('stringChecksumToNumber', () => {
+  TEST_SENTENCES.forEach((sentence, index) => {
+    const cs = sentence.split(DELIMITER)[1].slice(0, CHECKSUM_LENGTH)
+    const checksum = stringChecksumToNumber(cs)
+    expect(checksum).toBe(EXPECTED_CHECKSUMS[index])
+  })
+})
+
+test('numberChecksumToString', () => {
+  TEST_SENTENCES.forEach((sentence, index) => {
+    const cs = sentence.split(DELIMITER)[1].slice(0, CHECKSUM_LENGTH)
+    const checksum = numberChecksumToString(EXPECTED_CHECKSUMS[index])
+    expect(checksum).toBe(cs)
+  })
+})
