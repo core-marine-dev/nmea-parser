f7ed05721d078e383f1caacd1701b91080fcdfd5
Cristóbal Contreras Rubio <crisconru@gmail.com>
2023-12-05 12:05:53 +0100

    parser tested

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,10 @@
 import { readdirSync } from 'node:fs'
 import Path from 'node:path'
-import { MAX_CHARACTERS } from "./constants";
-import { BooleanSchema, NaturalSchema } from "./schemas";
-import { NMEAParser, StoredSentences } from "./types";
+import { END_FLAG, END_FLAG_LENGTH, MAX_CHARACTERS, START_FLAG, START_FLAG_LENGTH } from "./constants";
+import { BooleanSchema, NMEALikeSchema, NaturalSchema, StringSchema } from "./schemas";
+import { Data, FieldType, NMEAKnownSentence, NMEAParser, NMEAPreParsed, NMEASentence, NMEAUknownSentence, ParserSentences, StoredSentence, StoredSentences } from "./types";
 import { getStoreSentences, readProtocolsFile } from './protocols';
+import { getNMEAUnparsedSentence } from './sentences';
 
 
 export class Parser implements NMEAParser {
@@ -11,7 +12,7 @@ export class Parser implements NMEAParser {
   protected _memory: boolean = true
   get memory() { return this._memory }
   set memory(mem: boolean) { this._memory = BooleanSchema.parse(mem) }
-  protected _buffer: string[] = []
+  protected _buffer: string = ''
   protected _bufferLength: number = MAX_CHARACTERS
   get bufferLimit() { return this._bufferLength }
   set bufferLimit(limit: number) { this._bufferLength = NaturalSchema.parse(limit) }
@@ -20,8 +21,8 @@ export class Parser implements NMEAParser {
   // get protocols() { return this._protocols }
   // Sentences
   protected _sentences: StoredSentences = new Map()
-  // get sentences() { return this._sentences.keys() }
-  
+  // get sentences() { return this._sentences }
+
   constructor(memory: boolean = false, limit: number = MAX_CHARACTERS) {
     this.memory = memory
     this.bufferLimit = limit
@@ -45,9 +46,9 @@ export class Parser implements NMEAParser {
     const { protocols } = readProtocolsFile(file)
     // Add to known protocols
     protocols.forEach(protocol => {
-      const protocalName = protocol.protocol
-      if (!this._protocols.includes(protocalName)) {
-        this._protocols.push(protocalName)
+      const protocolName = protocol.protocol
+      if (!this._protocols.includes(protocolName)) {
+        this._protocols.push(protocolName)
       }
     })
     // Get sentences for new protocols
@@ -56,16 +57,122 @@ export class Parser implements NMEAParser {
     this._sentences = new Map([...this._sentences, ...sentences])
   }
 
-  getSentences(): string[] {
-    return [...this._sentences.keys()]
+  getSentences(): ParserSentences {
+    return Object.fromEntries(this._sentences.entries())
   }
 
-  parseData(text: string): any[] {
-    // const parsed = StringSchema.safeParse(text)
-    // if (!parsed.success) return []
-    // const frames = this.getFrames(parsed.data)
-    // TODO:
-    return['TODO:']
+  private getField(value: string, type: FieldType): Data {
+    if (value.length === 0) return null
+
+    switch (type) {
+      case 'string':
+        return value
+      case 'bool':
+      case 'boolean':
+        return (Boolean(value)).valueOf()
+      case 'float':
+      case 'double':
+      case 'number': {
+        const number = parseFloat(value)
+        if (!Number.isNaN(number)) return number
+        throw new Error(`invalid float number -> ${value}} it is not an ${type}`)
+      }
+    }
+
+    const number = parseInt(value)
+    if (Number.isInteger(number)) return number
+    throw new Error(`invalid integer -> ${value}} it is not an ${type}`)
   }
 
+  private getUnknowFrame(sentence: NMEAPreParsed): NMEAUknownSentence {
+    return { ...sentence, protocol: { name: 'UNKNOWN' } }
+  }
+
+  private getKnownFrame(preparsed: NMEAPreParsed): NMEAKnownSentence | null {
+    const storedSentence = this._sentences.get(preparsed.sentence) as StoredSentence
+    // Bad known frame
+    if (storedSentence.fields.length !== preparsed.fields.length) {
+      console.debug(`Invalid ${preparsed.sentence} sentence -> it has to have ${storedSentence.fields.length} fields but it contains ${preparsed.fields.length}`)
+      return null
+    }
+    try {
+      const knownSentence: NMEAKnownSentence = {...preparsed, ...storedSentence, data: [] } as NMEAKnownSentence
+      preparsed.fields.forEach((value, index) => {
+        const type = knownSentence.fields[index].type
+        const data = this.getField(value, type)
+        knownSentence.fields[index].data = data
+        knownSentence.data.push(data)
+      })
+      return knownSentence
+    } catch (error) {
+      if (error instanceof Error) {
+        console.debug(`Invalid NMEA Frame ${preparsed.sentence} -> ${preparsed.raw}\n\t${error.message}`)
+      } else {
+        console.error(error)
+      }
+    }
+    return null
+  }
+
+  private getFrame(text: string, timestamp: number): NMEASentence | null {
+    const unparsedSentence = getNMEAUnparsedSentence(text)
+    // Not valid NMEA sentence
+    if (unparsedSentence === null) {
+      console.debug(`Invalid NMEA frame -> ${text}}`)
+      return null
+    }
+    const preparsedSentence: NMEAPreParsed = { timestamp, ...unparsedSentence }
+    // Unknown NMEA sentence
+    if (!this._sentences.has(preparsedSentence.sentence)) {
+      console.debug(`Unknown NMEA sentence -> ${text}`)
+      return this.getUnknowFrame(preparsedSentence)
+    }
+    // Known NMEA sentence
+    return this.getKnownFrame(preparsedSentence)
+  }
+
+  private getFrames(text: string): NMEASentence[] {
+    const timestamp = Date.now()
+    let frames: NMEASentence[] = []
+    let pivot = 0
+
+    while (pivot < text.length) {
+      const start = text.indexOf(START_FLAG, pivot)
+      if (start === -1) {
+        this._buffer = ''
+        break
+      }
+
+      const end = text.indexOf(END_FLAG, start + START_FLAG_LENGTH)
+      if (end === -1) {
+        if (this._memory) { this._buffer = text.slice(start) }
+        break
+      }
+
+      const possibleFrame = text.slice(start, end + END_FLAG_LENGTH)
+      const parsed = NMEALikeSchema.safeParse(possibleFrame)
+
+      if (!parsed.success) {
+        pivot = start + START_FLAG_LENGTH
+        continue
+      }
+
+      const frame = this.getFrame(parsed.data, timestamp)
+      if (frame === null) {
+        pivot = start + START_FLAG_LENGTH
+        continue
+      }
+      
+      frames.push(frame)
+      pivot = end + END_FLAG_LENGTH
+    }
+    return frames
+  }
+
+  parseData(text: string): NMEASentence[] {
+    const parsed = StringSchema.safeParse(text)
+    if (!parsed.success) return []
+    const data = (this.memory) ? this._buffer + parsed.data : parsed.data
+    return this.getFrames(data)
+  }
 }
--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,15 +1,131 @@
+import path from 'node:path'
 import { describe, test, expect } from 'vitest'
 import { Parser } from '../src/parser'
+import { generateSentence } from '../src/sentences'
+import { NMEAKnownSentenceSchema, NMEAUknownSentenceSchema } from '../src/schemas'
+import { DELIMITER, END_FLAG, END_FLAG_LENGTH, SEPARATOR, START_FLAG_LENGTH } from '../src/constants'
+import { getChecksum, numberChecksumToString } from '../src/checksum'
+
+const NORSUB_FILE = path.join(__dirname, 'norsub.yaml')
 
 describe('Parser', () => {
   test('Default constructor', () => {
     const parser = new Parser()
-    const expectedProtocols = ['NMEA', 'GYROCOMPAS1', 'NORSUB8']
     const parserProtocols = parser.getProtocols()
-    expectedProtocols.forEach(protocol => expect(parserProtocols.includes(protocol)).toBeTruthy())
+    expect(parserProtocols.includes('NMEA')).toBeTruthy()
 
-    const expectedSentences = ['HEHDT', 'PHTRO', 'PHINF']
     const parserSentences = parser.getSentences()
-    expectedSentences.forEach(sentence => expect(parserSentences.includes(sentence)).toBeTruthy())
+    const expectedSentences = ['AAM', 'GGA']
+    expectedSentences.forEach(sentence => expect(Object.keys(parserSentences).includes(sentence)).toBeTruthy())
+  })
+  
+  test('Add protocols', () => {
+    const parser = new Parser()
+    parser.addProtocols(NORSUB_FILE)
+
+    const parserProtocols = parser.getProtocols()
+    
+    const expectedProtocols = [
+      'NMEA',
+      'GYROCOMPAS1', 'Tokimek PTVG', 'RDI ADCP', 'SMCA', 'SMCC',
+      'NORSUB', 'NORSUB2', 'NORSUB6', 'NORSUB7', 'NORSUB7b', 'NORSUB8', 'NORSUB PRDID',
+    ]
+    expectedProtocols.forEach(protocol => {
+      const result = parserProtocols.includes(protocol)
+      if (!result) { console.log(`Protocol ${protocol} is not included`) }
+      expect(result).toBeTruthy()
+    })
+  
+    const parserSentences = parser.getSentences()
+    const expectedSentences = [
+      'AAM', 'GGA',
+      'HEHDT', 'PHTRO', 'PHINF',
+      'PNORSUB', 'PNORSUB2', 'PNORSUB6', 'PNORSUB7', 'PNORSUB7b', 'PNORSUB8', 'PRDID',
+      'PTVG', 'PRDID', 'PSMCA', 'PSMCC',
+    ]
+    expectedSentences.forEach(sentence => {
+      const result = Object.keys(parserSentences).includes(sentence)
+      if (!result) { console.log(`Sentence ${sentence} is not included`) }
+      expect(result).toBeTruthy()
+    })
+  })
+
+  test('Parsing NMEA + NorSub sentences', () => {
+    const parser = new Parser()
+    parser.addProtocols(NORSUB_FILE)
+    const storedSentences = parser.getSentences()
+    Object.values(storedSentences).forEach(storedSentence => {
+      const input = generateSentence(storedSentence)
+      expect(input).toBeTypeOf('string')
+      const output = parser.parseData(input)[0]
+      expect(() => NMEAKnownSentenceSchema.parse(output)).not.toThrow()
+    })
+  })
+
+  test('Uncompleted frames WITHOUT memory', () => {
+    const parser = new Parser()
+    const storedSentences = parser.getSentences()
+    const input1 = generateSentence(storedSentences['AAM'])
+    const halfInput1 = input1.slice(0, 10)
+    const halfInput2 = input1.slice(10)
+    const input2 = generateSentence(storedSentences['GGA']);
+    [
+      halfInput1 + input2,
+      halfInput1 + halfInput1+ input2,
+      input2 + halfInput2,
+      input2 + halfInput2 + halfInput2,
+      'asdfasfaf' + input2 + 'lakjs'
+    ].forEach(input => {
+      const output = parser.parseData(input)
+      expect(output).toHaveLength(1)
+    })
+  })
+
+  test('Uncompleted frames WITH memory', () => {
+    const parser = new Parser()
+    parser.memory = true
+    const storedSentences = parser.getSentences()
+    const input1 = generateSentence(storedSentences['AAM'])
+    const halfInput1 = input1.slice(0, 10)
+    const halfInput2 = input1.slice(10)
+    const input2 = generateSentence(storedSentences['GGA']);
+    [
+      halfInput1 + input2,
+      halfInput1 + halfInput1+ input2,
+      input2 + halfInput2,
+      input2 + halfInput2 + halfInput2,
+      'asdfasfaf' + input2 + 'lakjs'
+    ].forEach(input => {
+      const output = parser.parseData(input)
+      expect(output).toHaveLength(1)
+    })
+    parser.parseData(halfInput1)
+    const mem = parser.parseData(halfInput2)
+    expect(mem).toHaveLength(1)
+  })
+
+  test('Unknown frames', () => {
+    const getFakeSentece = (text: string, sentence: string): string => {
+      const [frame, _cs] = text.slice(START_FLAG_LENGTH, -END_FLAG_LENGTH).split(DELIMITER)
+      const [_emitter, ...info] = frame.split(SEPARATOR)
+      const newFrame = [sentence, ...info].join(SEPARATOR)
+      const checksum = numberChecksumToString(getChecksum(newFrame))
+      return `$${newFrame}${DELIMITER}${checksum}${END_FLAG}`
+    }
+
+    const parser = new Parser()
+    const storedSentences = parser.getSentences()
+    const aam = storedSentences['AAM']
+    const gga = storedSentences['GGA']
+    const input1 = getFakeSentece(generateSentence(aam), 'XXX')
+    const input2 = getFakeSentece(generateSentence(gga), 'YYY');
+    [input1, input2].forEach(input => {
+      const output = parser.parseData(input)[0]
+      const parsed = NMEAUknownSentenceSchema.safeParse(output)
+      if (!parsed.success) {
+        console.error(parsed.error)
+      }
+      expect(parsed.success).toBeTruthy()
+    })
   })
 })
